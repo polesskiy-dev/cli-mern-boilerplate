46672b9b91ae0d8464c88d1a8324588a314d0a6a
Artem Polisskyi <polesskiy.dev@gmail.com>
2016-10-12 18:25:08 +0300

    Start refactoring, move boilerplate files to separate src directory

--- cli/index.js
+++ cli/index.js
@@ -4,30 +4,35 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const program = require('commander');
-let pkg = require('../package.json');
-const boilerplateLink = "/usr/local/lib/node_modules/" + Object.keys(pkg.bin)[0];
+const boilerplateLink = "/usr/local/lib/node_modules/" + Object.keys(require('../package.json').bin)[0] + "/src";
+let packageTemplate = require('../src/package.json');
 
 /** init node readline*/
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "cli-mern-boilerplate: "
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "cli-mern-boilerplate: "
 });
 
 const askName = ()=> new Promise((resolve)=> rl.question("What's your name? ", answer => resolve(answer)));
 const askEmail = ()=> new Promise((resolve)=> rl.question("What's your email? ", answer => resolve(answer)));
-const askLooksPretty = ()=> new Promise((resolve)=> rl.question(JSON.stringify(pkg, null, " ") + "\r\nIs it looks pretty Yes/no?", answer => resolve(answer)));
+const askLooksPretty = ()=> new Promise((resolve)=> rl.question(JSON.stringify(packageTemplate, null, " ") + "\r\nIs it looks pretty Yes/no?", answer => resolve(answer)));
 
 /**
  * Add CLI arguments
  */
 program
-  .command('init [directory]')
-  .option('-n, --name', 'Create application with name')
-  .action(init);
+    .command('init [directory]')
+    .description('Create application')
+    .option('-n, --name', 'Name of new application')
+    .action(init)
 
 program
-  .parse(process.argv); // end with parse to parse through the input
+    .command('i')
+    .action(()=>console.log("hello"))
+
+program
+    .parse(process.argv); // end with parse to parse through the input
 
 /**
  * Init app in directory appName
@@ -36,27 +41,27 @@ program
  * @returns {Promise.<TResult>}
  */
 function init(appName) {
-  const destinationFolder = `${path.resolve()}/${appName}/`;
-  console.log("MERN boilerplate will be initialized in directory: %s", destinationFolder);
-  pkg.name = appName;
-  delete pkg.repository;
+    const destinationFolder = `${path.resolve()}/${appName}/`;
+    console.log("MERN boilerplate will be initialized in directory: %s", destinationFolder);
+    packageTemplate.name = appName;
+    delete packageTemplate.repository;
 
-  return (
-    askName()
-      .then((name)=> pkg.author.name = name)
-      .then(askEmail)
-      .then((email)=>pkg.author.email = email)
-      .then(askLooksPretty)
-      //if answer not "yes" or empty - start init from beginning
-      .then((answer)=>(answer.trim() === 'yes' || '\r' ? '' : init(appName)))
-      .then(()=> {
-        rl.close();
-        process.stdin.destroy();
-        console.log(pkg);
-      })
-      .then(()=>copyDirRecursively(boilerplateLink, destinationFolder, /^package.json$|^node_modules$|^cli$|^.git(?!.*ignore)|^.idea/))
-      .then(()=>fs.writeFile(destinationFolder + '/package.json', JSON.stringify(pkg, null, ' ')))
-  )
+    return (
+        askName()
+            .then((name)=> packageTemplate.author.name = name)
+            .then(askEmail)
+            .then((email)=>packageTemplate.author.email = email)
+            .then(askLooksPretty)
+            //if answer not "yes" or empty - start init from beginning
+            .then((answer)=>(answer.trim() === 'yes' || '\r' ? '' : init(appName)))
+            .then(()=> {
+                rl.close();
+                process.stdin.destroy();
+                console.log(packageTemplate);
+            })
+            .then(()=>copyDirRecursively(boilerplateLink, destinationFolder, /^package.json$|^node_modules$|^cli$|^.git(?!.*ignore)|^.idea/))
+            .then(()=>fs.writeFile(destinationFolder + '/package.json', JSON.stringify(packageTemplate, null, ' ')))
+    )
 }
 
 /**
@@ -67,32 +72,32 @@ function init(appName) {
  * @param {RegExp} filter - ignore this names
  */
 function copyDirRecursively(src, dest, filter) {
-  /** remove destination directory if exists and creates new one*/
-  try {
-    fs.accessSync(dest);
-    fs.rmdirSync(dest);
-    fs.mkdirSync(dest);
-  } catch (e) {
-    fs.mkdirSync(dest);
-  }
-
-  //obtain all file names from src directory
-  const fileNames = fs.readdirSync(src);
-
-  /** iterate through all files and folders in src dir */
-  for (const fileName of fileNames) {
-    if (filter instanceof RegExp && !filter.test(fileName)) {
-      const filePath = src + '/' + fileName;
-      const isFile = fs.statSync(filePath).isFile();
-
-      console.log(`Copying ${isFile ? "file" : "dir"} ${fileName} to ${dest}`);
-
-      if (isFile)
-        fs.createReadStream(filePath).pipe(fs.createWriteStream(dest + '/' + fileName));
-      else
-        copyDirRecursively(filePath, dest + '/' + fileName, filter)
+    /** remove destination directory if exists and creates new one*/
+    try {
+        fs.accessSync(dest);
+        fs.rmdirSync(dest);
+        fs.mkdirSync(dest);
+    } catch (e) {
+        fs.mkdirSync(dest);
     }
-  }
+
+    //obtain all file names from src directory
+    const fileNames = fs.readdirSync(src);
+
+    /** iterate through all files and folders in src dir */
+    for (const fileName of fileNames) {
+        if (filter instanceof RegExp && !filter.test(fileName)) {
+            const filePath = src + '/' + fileName;
+            const isFile = fs.statSync(filePath).isFile();
+
+            console.log(`Copying ${isFile ? "file" : "dir"} ${fileName} to ${dest}`);
+
+            if (isFile)
+                fs.createReadStream(filePath).pipe(fs.createWriteStream(dest + '/' + fileName));
+            else
+                copyDirRecursively(filePath, dest + '/' + fileName, filter)
+        }
+    }
 }
 
 
